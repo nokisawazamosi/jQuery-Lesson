52fd18c5396f2119f902a64fd02232304c745e6c
EC2 Default User <ec2-user@ip-172-31-20-101.ap-northeast-1.compute.internal>
2023-08-05 00:24:46 +0000

    [Finish]javascript-7

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-$(function (){
-  $('.box1').on('click', function (){
-    $('.box1').addClass('box1-ext');
-   });
-  $('.box1').mouseout(function (){
-    $('.box1').removeClass('box1-ext');
-   });
-});
+// $(function (){
+//   $('.box1').on('click', function (){
+//     $('.box1').addClass('box1-ext');
+//   });
+//   $('.box1').mouseout(function (){
+//     $('.box1').removeClass('box1-ext');
+//   });
+// });
 
+// $(function(){
+//   $('.box1').on('click', function(){
+//     $(this).slideUp();
+//   });
+// });
+
+$(function(){
+  $('button').on('click', function(){
+    $('ul').children().css('color','red');
+  });
+});
